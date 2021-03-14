ca4cd814246c36ecc207394347ebbebb53e332f6
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-14 14:49:21 -0400

    Support for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter, 
+  Route, 
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from 'components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
