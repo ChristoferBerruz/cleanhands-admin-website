9a80c500bae346b758fbe8546692b479c5c36dd4
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-16 21:50:41 -0400

    Simple footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer:React.FC = () =>{
+    return (
+        <footer className="footer">
+            <div className="content has-text-centered">
+                <p>
+                <strong>Cleanhands</strong> by Anastasiia Babenko and Christofer Berruz
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
