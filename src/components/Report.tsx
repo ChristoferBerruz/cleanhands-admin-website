5ac94842fe1bce1957024be49c6559660279b01f
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-03-16 21:51:27 -0400

    Maybe we should create branches lol

---
+++ src/components/Report.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+
+const ReportTable:React.FC = () => {
+    return(
+        <div className="section">
+            <div className="container">
+            <table className="table is-bordered">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Average Handwashing time</th>
+                        <th>Minimum time</th>
+                        <th>Maximum time</th>
+                        <th>No. of users</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+            </div>
+        </div>
+    )
+}
+const ReportContent:React.FC = () => {
+    return(
+        <>
+            <ReportTable />
+        </>
+    )
+}
+const Report:React.FC = () => {
+    return(
+        <>
+            <Header />
+            <ReportContent />
+            <Footer />
+        </>
+    )
+}
+
+export default Report;
