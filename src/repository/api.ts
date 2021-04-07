5de01f9ec62cd41cb8396ca5104b70c47a902bab
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-04-07 17:07:54 -0400

    Added first function: "login" that queries api.

--- src/repository/api.ts
+++ src/repository/api.ts
@@ -1,4 +1,23 @@
 import { ChartData } from 'chart.js';
+import axios from 'axios';
+//const axios = require('axios').default;
+
+const baseURL: string = 'http://64.225.55.171:5000/api/v1';
+interface LoginBody {
+    [key: string]: string;
+}
+
+//axios.get('some api url', {withCredentials: true});
+
+export function tryLogin(email: string, password: string) {
+    let data: LoginBody = { email: email, password: password };
+    axios
+        .post(`${baseURL}/login`, data)
+        .then((res: any) => alert(res))
+        .catch((err: any) => {
+            alert('Something went wrong..' + err);
+        });
+}
 
 export async function getDeviceData(): Promise<ChartData[]> {
     return new Promise<ChartData[]>((resolve, reject) => {
@@ -41,7 +60,9 @@ export async function getDeviceData(): Promise<ChartData[]> {
         };
 
         const dataArray: ChartData[] = [data0, data1];
-
+        /*if (wrong) {
+            reject(Error('Bad input'));
+        }*/
         setTimeout(() => resolve(dataArray), 5000);
     });
 }
