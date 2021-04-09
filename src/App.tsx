c5cae3fb45d5af58e2983ed2bad4a6d30feb2e33
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-04-09 16:49:53 -0400

    Moved LoggedInProvider to be initialized in App to give Router access to isLoggedIn flag and restrict access to certain routes to unauthenticated users.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -9,24 +9,33 @@ import Report from 'components/Report';
 import Login from 'components/Login';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-import LoggedInProvider from 'components/IsLoggedInContext';
+import { IsLoggedInContext } from 'components/IsLoggedInContext';
 
 function App() {
+    const [isLoggedIn, setLoginStatus] = useState(false);
+    const initial = { isLoggedIn, setLoginStatus };
     return (
         <div className="App">
-            <LoggedInProvider>
+            <IsLoggedInContext.Provider value={initial}>
                 <Header />
                 <BrowserRouter>
                     <Switch>
                         <Route path="/" exact component={HomeContent} />
-                        <Route path="/profile" component={Profile} />
-                        <Route path="/statistics" component={Statistics} />
-                        <Route path="/report" component={Report} />
                         <Route path="/login" component={Login} />
+                        {isLoggedIn && (
+                            <Route path="/profile" component={Profile} />
+                        )}
+                        {isLoggedIn && (
+                            <Route path="/statistics" component={Statistics} />
+                        )}
+                        {isLoggedIn && (
+                            <Route path="/report" component={Report} />
+                        )}
+                        <Route render={() => <Redirect to="/" />} />
                     </Switch>
                 </BrowserRouter>
                 <Footer />
-            </LoggedInProvider>
+            </IsLoggedInContext.Provider>
         </div>
     );
 }
