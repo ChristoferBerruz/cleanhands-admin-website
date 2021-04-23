56db50ac9976102d6a89440c39735c00a7b5d0c0
Christofer Berruz <cberruzc@my.bridgeport.edu>
2021-04-23 11:02:22 -0400

    Final commit?

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
+import endHandwashing from 'assets/End Handwashing.jpg';
+import startHandwashing from 'assets/Start Handwashing.jpg';
+import wristAppears from 'assets/Wrist Appears.jpg';
+import wristDisappears from 'assets/Wrist Disappears.jpg';
 
+const HandwashingCard: React.FC<{ imagePath: string; description: string }> = ({
+    imagePath,
+    description,
+}) => {
+    return (
+        <div className="box">
+            <figure className="image">
+                <img src={imagePath} />
+            </figure>
+            <p>{description}</p>
+        </div>
+    );
+};
+
+const HandwashingCards: React.FC = () => {
+    const images = [
+        wristAppears,
+        startHandwashing,
+        endHandwashing,
+        wristDisappears,
+    ];
+    const descriptions = [
+        'Wrist Appears',
+        'Starts handwashing',
+        'Ends handwashing',
+        'Wrist disappears',
+    ];
+
+    let cards = images.map((image, idx) => (
+        <div className="column is-3">
+            <HandwashingCard
+                key={idx}
+                imagePath={image}
+                description={descriptions[idx]}
+            />
+        </div>
+    ));
+    return <div className="columns">{cards}</div>;
+};
 const HomeContent: React.FC = () => {
     return (
-        <div className="hero">
-            <div className="hero-body">
-                <p className="title">Cleanhands</p>
-                <p className="subtitle">Hero subtitle</p>
+        <div className="container">
+            <div className="hero">
+                <div className="hero-body">
+                    <p className="title">Cleanhands</p>
+                    <p className="subtitle">
+                        Improving hand hygiene one video a time.
+                    </p>
+                    <HandwashingCards />
+                </div>
             </div>
         </div>
     );
